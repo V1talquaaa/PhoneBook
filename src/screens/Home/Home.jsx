2bbad870588dea0f35912a905978ae3ad9d7bc05
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-07-14 15:19:34 +0200

    Update Home.jsx

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -3,7 +3,7 @@ export const Home = () => {
     return(
         <div className={css.home}>
         <h2>PhoneBook</h2>
-        <p>Manage, Customize, and Connect. Effortlessly add, remove, and filter contacts with our intuitive tool. Keep your contact list organized and stay connected with ease. Simplify your life and streamline your communication with PhoneBook Too</p>
+        <p>Manage, Customize, and Connect. Effortlessly add, remove, and filter contacts with our intuitive tool. Keep your contact list organized and stay connected with ease. Simplify your life and streamline your communication with PhoneBook Tool</p>
         </div>
     )
 }
